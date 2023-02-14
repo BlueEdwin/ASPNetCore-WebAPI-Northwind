c8fdf4cd09d69ac3bde3ee91ff7c1d682113cfef
edwang <wangedwin1102@gmail.com>
2023-02-14 21:16:19 +0800

    建立IGenericRepository

---
+++ Repository/IGenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    /// <summary>
+    /// A Generic Repository Interface
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// 新增
+        /// </summary>
+        /// <param name="entity"></param>
+        void Add(TEntity entity);
+
+        /// <summary>
+        /// 取得全部
+        /// </summary>
+        /// <returns></returns>
+        Task<ICollection<TEntity>> GetAllAsync();
+
+        /// <summary>
+        /// 取得單筆
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate);
+
+        /// <summary>
+        /// 刪除
+        /// </summary>
+        /// <param name="entity"></param>
+        void Remove(TEntity entity);
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="entity"></param>
+        void Update(TEntity entity);
+    }
+}
