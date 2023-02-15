26e7a9695b3416a29c54ce940b32cbb8f0a521c8
edwang <wangedwin1102@gmail.com>
2023-02-15 20:34:22 +0800

    加入IUnitOfWork

---
+++ Repository/IUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        /// <summary>
+        /// DbContext
+        /// </summary>
+        DbContext Context { get; }
+        
+        /// <summary>
+        /// Saves the change
+        /// </summary>
+        /// <returns></returns>
+        Task<int> SaveChangeAsync();
+
+        IGenericRepository<TEntity> Repository<TEntity>() where TEntity : class;
+    }
+}
---
+++ Repository/UnitOfWork.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private bool disposed = false;
+        private Hashtable _repositories;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnitOfWork"/> class.
+        /// </summary>
+        /// <param name="context"></param>
+        public UnitOfWork(DbContext context) 
+        {
+            this.Context = context;
+        }
+        public UnitOfWork()
+        {
+
+        }
+        /// <summary>
+        /// DbContext
+        /// </summary>
+        public DbContext Context { get; set; }
+        /// <summary>
+        /// Dispose
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        /// <summary>
+        /// 取得某一個Entity的Repository，
+        /// 如果沒有取過就會initialize一個，
+        /// 如果有就取得之前initialize的那個
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <returns></returns>
+        public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : class
+        {
+            if(_repositories == null)
+            {
+                _repositories = new Hashtable();
+            }
+
+            var type = typeof(TEntity).Name;
+
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(GenericRepository<>);
+
+                var repositoryInstance = Activator.CreateInstance(repositoryType
+                                                        .MakeGenericType(typeof(TEntity)), Context);
+                _repositories.Add(type, repositoryInstance);
+            }
+
+            return (IGenericRepository<TEntity>)_repositories[type];
+        }
+        /// <summary>
+        /// Save Change
+        /// </summary>
+        /// <returns></returns>
+        public async Task<int> SaveChangeAsync()
+        {
+            return await this.Context.SaveChangesAsync();
+        }
+        /// <summary>
+        /// Dispose
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    this.Context.Dispose();
+                    this.Context = null;
+                }
+            }
+
+            this.disposed = true;
+        }
+    }
+}
