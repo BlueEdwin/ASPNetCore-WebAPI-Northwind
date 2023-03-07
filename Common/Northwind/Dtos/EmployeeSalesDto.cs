db7a2877caa645f36186ba4dcf493f2784ee0272
EdwinWang1102 <wangedwin1102@gmail.com>
2023-03-07 19:59:42 +0800

    實作查詢員工某年度銷售數量

---
+++ Common/Northwind/Dtos/EmployeeSalesDto.cs
@@ -0,0 +1,20 @@
+﻿using Common.Northwind.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Northwind.Dtos
+{
+    public class EmployeeSalesDto
+    {
+        public int EmployeeID { get; set; }
+        public string LastName { get; set; } = null!;
+
+        public string FirstName { get; set; } = null!;
+        public string? City { get; set; }
+        public virtual ICollection<Order> Orders { get; } = new List<Order>();
+        public  int OrderNumber { get; set; }
+    }
+}
--- Service/Northwind/NorthwindService.cs
+++ Service/Northwind/NorthwindService.cs
@@ -4,7 +4,9 @@ using Common.Northwind.Entities;
 using Repository.Northwind;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,6 +30,41 @@ namespace Service.Northwind
             IEnumerable<ProductDto> productDtos = _mapper.Map<IEnumerable<ProductDto>>(products);
 
             return productDtos;
+        }
+        public IEnumerable<EmployeeSalesDto> GetEmployeeSales(QueryEmployeeSalesDto queryParam)
+        {
+            try
+            {
+                DateTime orderYear = DateTime.ParseExact(queryParam.Year.ToString(), "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture);
+
+                DateTime orderYearEnd = orderYear.AddYears(1);
+
+                IEnumerable<Order> orders = _unitOfWork.Repository<Order>().GetAllAsync().Result.Where(x => x.OrderDate >= orderYear && x.OrderDate <= orderYearEnd);
+
+                IEnumerable<Order_Detail> orderDetails = _unitOfWork.Repository<Order_Detail>().GetAllAsync().Result;
+
+                IEnumerable<Employee> employees = _unitOfWork.Repository<Employee>().GetAllAsync().Result;
+
+                IEnumerable<EmployeeSalesDto> empSales = from o in orders
+                                                         join or in orderDetails on o.OrderID equals or.OrderID
+                                                         join e in employees on o.EmployeeID equals e.EmployeeID
+                                                         select new EmployeeSalesDto
+                                                         {
+                                                             EmployeeID = e.EmployeeID,
+                                                             FirstName = e.FirstName,
+                                                             LastName = e.LastName,
+                                                             City = e.City,
+                                                             OrderNumber = (from o in orders
+                                                                            where o.EmployeeID == e.EmployeeID
+                                                                            select o).Count()
+                                                         };
+
+                return empSales;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
